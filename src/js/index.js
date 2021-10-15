2c5e5952cf078278b68c8a59d88f3a828826f6f8
Favour Samuel <sfchidimma@gmail.com>
2021-10-15 14:10:08 +0100

    Include main JS content

--- src/js/index.js
+++ src/js/index.js
@@ -1,77 +1,90 @@
 // Copyright 2021 sfchi
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todosUl = document.getElementById('todos')
+const addForm = document.getElementById("add");
+const lists = document.querySelector(".todos");
+const searchInput = document.getElementById("search-input");
 
-const todos = JSON.parse(localStorage.getItem('todos'))
+const generateTemplate = (todo) => {
+    const html = `
+     <li class="list-item">
+      <span class="">${todo}</span>
+      <i class="far fa-trash-alt delete"></i>
+    </li>
+  `;
 
-if(todos) {
-    todos.forEach(todo => addTodo(todo))
-}
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+    lists.innerHTML += html;
+};
 
-    addTodo()
-})
+// Add new todo
+function addTodo() {
+    addForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const todo = addForm.add.value.trim();
 
-function addTodo(todo) {
-    let todoText = input.value
-
-    if(todo) {
-        todoText = todo.text
-    }
-
-    if(todoText) {
-        const todoElement = document.createElement('li')
-
-        if(todo && todo.completed) {
-            todoElement.classList.add('completed')
-            updateLS()
+        if (todo.length) {
+            generateTemplate(todo);
+            addForm.reset();
         }
 
-        todoElement.innerText = todoText
+        updateLS();
+    });
+}
 
-        todoElement.addEventListener('click', () => todoElement.classList.toggle('completed'))
-
-        todoElement.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-
-            todoElement.remove()
-            updateLS()
-        })
-
-        todosUl.appendChild(todoElement)
-
-        input.value = ''
-
-        updateLS()
+// Delete Todo
+lists.addEventListener("click", (e) => {
+    if (e.target.classList.contains("delete")) {
+        e.target.parentElement.remove();
     }
+    updateLS();
+});
+
+// Filter List
+const searchBar = document.forms["search-form"].querySelector("input");
+
+searchBar.addEventListener("keyup", (e) => {
+    const term = e.target.value.toLowerCase();
+    const listItem = lists.getElementsByTagName("li");
+
+    Array.from(listItem).forEach((list) => {
+        const title = list.firstElementChild.textContent;
+
+        if (title.toLowerCase().indexOf(term) !== -1) {
+            list.style.display = "block";
+        } else {
+            list.style.display = "none";
+        }
+    });
+});
+
+// Add to Local Storage
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+if (todos) {
+    todos.forEach((todo) => addTodo(todo));
 }
 
 function updateLS() {
-    todosElement = document.querySelectorAll('li')
+    const todosElement = document.querySelectorAll("li");
 
-    const todos = []
+    const todos = [];
 
-    todosElement.forEach(todoElement => {
+    todosElement.forEach((todoElement) => {
         todos.push({
             text: todoElement.innerText,
-            completed: todoElement.classList.contains('completed')
-        })
-    })
+        });
+    });
 
-    localStorage.setItem('todos', JSON.stringify(todos))
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
